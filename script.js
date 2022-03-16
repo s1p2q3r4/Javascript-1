b94cdb57d2a7b7610e668f09e1b231755926213e
s1p2q3r4 <99183896+s1p2q3r4@users.noreply.github.com>
2022-03-16 13:16:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var nam= "Hello world" 
+console.log(nam)
+var z=36
+console.log(z)
+var y=4.6
+console.log(y)
+var x=true
+console.log(x)
+
+
+//Circumference of the circle
+var p=2
+var q=3.14
+var r=2
+const c=p*q*r
+console.log(c)
+
+//area of the circle
+var m=3.14
+var n=3
+const a=m*n*n
+console.log(a)
+//radius of the circle
+let s
+s=4
+console.log(s)
